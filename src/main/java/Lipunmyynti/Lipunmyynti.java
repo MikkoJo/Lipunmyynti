05f8ebc52cfdfe57695f3c445272e9ada5e04ecc
Natalia Baldina <Natalia.Baldina@tite.amiedu-ict.net>
2019-10-10 11:27:08 +0300

    Ku-ku

---
+++ src/main/java/Lipunmyynti/Lipunmyynti.java
@@ -0,0 +1,13 @@
+package Lipunmyynti;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lipunmyynti 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Lipunmyynti/Paasylippu.java
@@ -0,0 +1,5 @@
+package Lipunmyynti;
+
+public class Paasylippu {
+
+}
