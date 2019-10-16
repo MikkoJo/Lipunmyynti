5562972256ee3be22705c374a158810101b57628
= <fadi.el-khouri@hotmail.com>
2019-10-16 13:11:02 +0300

    lisaa

--- src/test/java/ProfiiliTest.java
+++ src/test/java/ProfiiliTest.java
@@ -1,17 +1,38 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class ProfiiliTest {
+	Profiili profiili;
+	Profiili profiili1;
 
 	@Before
 	public void setUp() throws Exception {
+		profiili =new Profiili("Henkilo", 40);
+		profiili1 =new Profiili("opiskelija", 40);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getIkaTest() {
+		profiili.getIka();
+		assertEquals(40, profiili.getIka());
+	}
+	@Test
+	public void getNimiTest() {
+		profiili.getStatus();
+		assertEquals("Henkilo", profiili.getStatus());
+	}
+	@Test
+	public void getAlennus() {
+		profiili.getAlennusProsentti();
+		assertEquals(0, profiili.getAlennusProsentti());
+	}
+	@Test
+	public void getAlennusOpiskelija() {
+		profiili1.getAlennusProsentti();
+		assertEquals(25.0, profiili1.getAlennusProsentti(),0.00001);
 	}
 
 }
